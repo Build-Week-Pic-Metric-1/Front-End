ead86c909ed97af323df265d3d7450758f0073d8
Vincent Sanders <vincesanders@hotmail.com>
2019-11-18 21:16:42 -0600

    Worked on the animation and javascript for the expanding sidebar.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 const hamBtn = document.getElementById("hamburger");
+const expandedMenu = document.querySelector(".expanded-sidebar");
+let expanded = false;
 
 hamBtn.addEventListener("click", function(event) {
-    let expandedMenu = document.querySelector(".expanded-sidebar");
-    if (expandedMenu.style.display === "none") {
-        expandedMenu.style.display = "flex";
+    if (expanded === false) {
+        expandedMenu.style.width = "20%";
+        hamBtn.innerHTML = '<i class="fas fa-times"></i>';
+        hamBtn.style.transform = 'scale(1.25,1.25)';
+        expanded = true;
     } else {
-        expandedMenu.style.display = "none";
+        expandedMenu.style.width = "0";
+        hamBtn.innerHTML = '<i class="fas fa-bars"></i>';
+        hamBtn.style.transform = 'scale(1.75,1)';
+        expanded = false;
     }
 });
