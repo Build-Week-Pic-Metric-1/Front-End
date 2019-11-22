51c436808dd1ff177722b954b74247b07b034b68
Ryan State <rwstate@gmail.com>
2019-11-22 01:28:06 -0500

    remove old client

--- client/src/components/MetricChart.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MetricChart = () => {
-  //need some logic to pull either user stats or global stats depending on url
-  return <div>{/* implement chartJS here with fetched data */}</div>;
-};
-
-export default MetricChart;
--- client/src/utils/utils.js
+++
@@ -1,1 +0,0 @@
-//axiosWithAuth here
